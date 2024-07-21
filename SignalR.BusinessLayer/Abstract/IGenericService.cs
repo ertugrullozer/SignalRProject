e296894857de1be09f829bf16bc64fa294f4ed70
Ertugrulozer <162031787+ertugrullozer@users.noreply.github.com>
2024-07-21 16:03:08 +0300

    Bussines Abstract Servislerin tamamlanması

---
+++ SignalR.BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.Abstract
+{
+    public interface IGenericService<T> where T : class
+    {
+        void Tadd(T entity);
+        void Tupdate(T entity);
+        void Tdelete(T entity);
+        T TGetByID(int id);
+        List<T> TGetListAll();
+    }
+}
