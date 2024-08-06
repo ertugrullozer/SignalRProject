a1c6cab1fe567ec98c17e9cdc50fbc56b36126f8
Ertugrulozer <162031787+ertugrullozer@users.noreply.github.com>
2024-08-06 15:15:47 +0300

    AutoMapper

---
+++ SignalRApi/Mapping/BookingMappingDto.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.BookingDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Mapping
+{
+    public class BookingMappingDto:Profile
+    {
+        public BookingMappingDto()
+        {
+            CreateMap<Booking,ResultBookingDto>().ReverseMap();
+            CreateMap<Booking,CreateBookingDto>().ReverseMap();
+            CreateMap<Booking,UpdateBookingDto>().ReverseMap();
+            CreateMap<Booking, GetBookingDto>().ReverseMap();
+
+        }
+    }
+}
---
+++ SignalRApi/Mapping/DiscountMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.DiscontDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Mapping
+{
+    public class DiscountMapping:Profile
+    {
+        public DiscountMapping()
+        {
+            CreateMap<Discount,ResultDiscontDto>().ReverseMap();
+            CreateMap<Discount,CreateDiscountDto>().ReverseMap();
+            CreateMap<Discount,UpdateDiscountDto>().ReverseMap();
+            CreateMap<Discount,GetDiscountDto>().ReverseMap();
+        }
+    }
+}
