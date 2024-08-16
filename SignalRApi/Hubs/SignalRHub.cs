f5e48be45a6810611a53be18bd2ae98ceabc9297
Ertugrulozer <162031787+ertugrullozer@users.noreply.github.com>
2024-08-16 16:47:44 +0300

    SignalR ile istatistikleri revize etme

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -1,15 +1,28 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.BusinessLayer.Abstract;
 using SignalR.DataAccessLayer.Concrete;
 
 namespace SignalRApi.Hubs
 {
 	public class SignalRHub : Hub
 	{
-		SignalRContext context= new SignalRContext();
-		public async Task SendCAtegoryCount()
+		private readonly ICategoryService _categoryService;
+		private readonly IProductService _productService;
+        public SignalRHub(ICategoryService categoryService,IProductService productService)
+        {
+            _categoryService = categoryService;
+			_productService = productService;
+        }
+
+        public async Task SendCAtegoryCount()
 		{
-			var value = context.Categories.Count();
+			var value = _categoryService.TCategoryCount();
 			await Clients.All.SendAsync("ReciveCategoryCount",value);
+		}
+		public async Task SendProductCount()
+		{
+			var value2 =_productService.TProductCount();
+			await Clients.All.SendAsync("ReciveProductCount",value2);
 		}
 
 	}
