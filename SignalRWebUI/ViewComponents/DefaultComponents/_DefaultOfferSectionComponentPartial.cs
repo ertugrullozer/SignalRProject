bebe8cec6bfc5e3cb6ad41424144b6c76807f525
Ertugrulozer <162031787+ertugrullozer@users.noreply.github.com>
2024-08-18 15:03:58 +0300

    Tema Dahil Edildi ve revizeler yapıldı

---
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultOfferSectionComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.DefaultComponents
+{
+    public class _DefaultOfferSectionComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
